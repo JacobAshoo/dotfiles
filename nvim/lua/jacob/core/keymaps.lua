3add61c631b9a43258fb973473402a79ee315b41
jacob <jacobashoo@gmail.com>
2023-02-05 19:34:26 -0500

    more stuff

--- nvim/lua/jacob/core/keymaps.lua
+++ nvim/lua/jacob/core/keymaps.lua
@@ -1,4 +1,4 @@
- vim.g.mapleader = " "
+vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
@@ -20,3 +20,10 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- plugin keymaps
+
+--vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- maximize or unmax split window
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
